f0c1081af1bef8a3d071bec13a434bac6cf21ca1
Matt Snyder <slylock2@mac.com>
2022-02-21 21:24:13 -0600

    code created by AAA

--- MiniMec/Mecanum-sample/src/main/cpp/Robot.cpp
+++ MiniMec/Mecanum-sample/src/main/cpp/Robot.cpp
@@ -2,52 +2,85 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+#include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
-#include <frc/XboxController.h>
-#include <frc/filter/SlewRateLimiter.h>
+#include <frc/drive/MecanumDrive.h>
+#include <frc/motorcontrol/VictorSP.h>
+#include "AHRS.h"
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/DriverStation.h>
 
-#include "Drivetrain.h"
 
+/**
+ * This is a demo program showing how to use Mecanum control with the
+ * MecanumDrive class.
+ */
 class Robot : public frc::TimedRobot {
  public:
-  void AutonomousPeriodic() override {
-    DriveWithJoystick(false);
-    m_mecanum.UpdateOdometry();
+  void RobotInit() override {
+    // Invert the right side motors. You may need to change or remove this to
+    // match your robot.
+    m_frontRight.SetInverted(true);
+    m_rearRight.SetInverted(true);
+
+    try{
+        /***********************************************************************
+         * navX-MXP:
+         * - Communication via RoboRIO MXP (SPI, I2C) and USB.            
+         * - See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface.
+         * 
+         * navX-Micro:
+         * - Communication via I2C (RoboRIO MXP or Onboard) and USB.
+         * - See http://navx-micro.kauailabs.com/guidance/selecting-an-interface.
+         * 
+         * VMX-pi:
+         * - Communication via USB.
+         * - See https://vmx-pi.kauailabs.com/installation/roborio-installation/
+         * 
+         * Multiple navX-model devices on a single robot are supported.
+         ************************************************************************/
+        m_ahrs = new AHRS(frc::SPI::Port::kMXP);
+
+
+      } catch (std::exception &ex) {
+        std::string what_string = ex.what();
+        std::string err_msg("Error instantiating navX MXP:  " + what_string);
+        const char *p_err_msg = err_msg.c_str();
+        FRC_ReportError(0, "{}", p_err_msg); // frc::DriverStation::ReportError(p_err_msg);
+      }
   }
 
-  void TeleopPeriodic() override { DriveWithJoystick(true); }
+  void TeleopPeriodic() override {
+    /* Use the joystick X axis for lateral movement, Y axis for forward
+     * movement, and Z axis for rotation.
+     */
+    m_robotDrive.DriveCartesian(m_stick.GetY(), -m_stick.GetX(), -m_stick.GetZ(), m_ahrs->GetAngle());
+
+    bool reset_yaw_button_pressed = m_stick.GetRawButton(1);
+    if ( reset_yaw_button_pressed ) {
+      m_ahrs->ZeroYaw();  
+      }
+
+  }
 
  private:
-  frc::XboxController m_controller{0};
-  Drivetrain m_mecanum;
+  static constexpr int kFrontLeftChannel = 1; //1
+  static constexpr int kRearLeftChannel = 0; //0
+  static constexpr int kFrontRightChannel = 3; //3
+  static constexpr int kRearRightChannel = 2; //2
 
-  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0
-  // to 1.
-  frc::SlewRateLimiter<units::scalar> m_xspeedLimiter{3 / 1_s};
-  frc::SlewRateLimiter<units::scalar> m_yspeedLimiter{3 / 1_s};
-  frc::SlewRateLimiter<units::scalar> m_rotLimiter{3 / 1_s};
+  static constexpr int kJoystickChannel = 0;
 
-  void DriveWithJoystick(bool fieldRelative) {
-    // Get the x speed. We are inverting this because Xbox controllers return
-    // negative values when we push forward.
-    const auto xSpeed = -m_xspeedLimiter.Calculate(m_controller.GetLeftY()) *
-                        Drivetrain::kMaxSpeed;
+  frc::VictorSP m_frontLeft{kFrontLeftChannel};
+  frc::VictorSP m_rearLeft{kRearLeftChannel};
+  frc::VictorSP m_frontRight{kFrontRightChannel};
+  frc::VictorSP m_rearRight{kRearRightChannel};
+  frc::MecanumDrive m_robotDrive{m_frontLeft, m_rearLeft, m_frontRight,
+                                 m_rearRight};
 
-    // Get the y speed or sideways/strafe speed. We are inverting this because
-    // we want a positive value when we pull to the left. Xbox controllers
-    // return positive values when you pull to the right by default.
-    const auto ySpeed = -m_yspeedLimiter.Calculate(m_controller.GetLeftX()) *
-                        Drivetrain::kMaxSpeed;
+  frc::Joystick m_stick{0};
 
-    // Get the rate of angular rotation. We are inverting this because we want a
-    // positive value when we pull to the left (remember, CCW is positive in
-    // mathematics). Xbox controllers return positive values when you pull to
-    // the right by default.
-    const auto rot = -m_rotLimiter.Calculate(m_controller.GetRightX()) *
-                     Drivetrain::kMaxAngularSpeed;
-
-    m_mecanum.Drive(xSpeed, ySpeed, rot, fieldRelative);
-  }
+  AHRS *m_ahrs;
 };
 
 #ifndef RUNNING_FRC_TESTS
@@ -55,3 +88,4 @@ int main() {
   return frc::StartRobot<Robot>();
 }
 #endif
+
