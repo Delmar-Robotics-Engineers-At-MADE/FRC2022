dda0f51d54e1330ab5acb0e08c7edadddb1dccd8
Matt Snyder <slylock2@mac.com>
2022-02-28 18:19:06 -0600

    run 2 motors with leader/follower

--- Protobot/shooter-prototype/src/main/cpp/Robot.cpp
+++ Protobot/shooter-prototype/src/main/cpp/Robot.cpp
@@ -12,39 +12,30 @@
 
 using namespace frc;
 
-/**
- * This is a demo program showing the use of the DifferentialDrive class.
- * Runs the motors with tank steering.
- */
 class Robot : public frc::TimedRobot {
-  WPI_TalonFX m_leftMotor{0};
-  WPI_TalonFX m_rightMotor{12};
-  //frc::DifferentialDrive m_robotDrive{m_leftMotor, m_rightMotor};
-  frc::Joystick m_leftStick{0};
-  frc::Joystick m_rightStick{1};
-
-  double motorOutPercent = 0.0;
+private:
+  WPI_TalonFX mPortShooter{0};  // LEADER
+  WPI_TalonFX mStarShooter{15};
+  double mMotorOutPercent = 0.0;
 
  public:
   void RobotInit() override {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.SetInverted(true);
-    frc::SmartDashboard::PutNumber("motor output percentage", motorOutPercent);
+    frc::SmartDashboard::PutNumber("motor output percentage", mMotorOutPercent);
+
+    // one follower and one reversed
+    mStarShooter.Follow(mPortShooter);
+    mStarShooter.SetInverted(false);
+    mPortShooter.SetInverted(true);
     
   }
 
   void TeleopInit() override {
-    frc::SmartDashboard::PutNumber("motor output percentage", motorOutPercent);
+    frc::SmartDashboard::PutNumber("motor output percentage", mMotorOutPercent);
   }
 
   void TeleopPeriodic() override {
-    // Drive with tank style
-    //m_robotDrive.TankDrive(m_leftStick.GetY(), m_rightStick.GetY());
-    motorOutPercent = frc::SmartDashboard::GetNumber("motor output percentage", 0);
-    m_leftMotor.Set(ControlMode::PercentOutput, motorOutPercent);
-    // std::cout << "motor setpoint: " << motorOutPercent << std::endl;
+    mMotorOutPercent = frc::SmartDashboard::GetNumber("motor output percentage", 0);
+    mPortShooter.Set(ControlMode::PercentOutput, mMotorOutPercent);
   }
 };
 
